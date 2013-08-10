2e19e336bf7e4dbb9678a51d1e55f2a855b541c4
Michael Comella <michael.l.comella@gmail.com>
2013-08-09 19:26:27 -0700

    Exchange bug # for {{bug|#}}.

--- lazylink.py
+++ lazylink.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python2
 
 from argparse import ArgumentParser
+import re
+
+BUG_REG = re.compile(r'(bug\s?)([0-9]+)', re.IGNORECASE)
 
 def main():
     args = set_and_parse_args()
@@ -14,7 +17,23 @@ def set_and_parse_args():
     return parser.parse_args()
 
 def lazylink_file(f):
-    pass
+    for line in f:
+        match = BUG_REG.search(line)
+        if match is None:
+            print line
+            continue
+        bug_no = match.group(2)
+        desc = get_bugzilla_desc(bug_no)
+
+        out = line[:match.start(1)]
+        out += '{{Bug|' + bug_no + '}} - '
+        out += desc
+        out +=  ' ' + line[match.end(2):]
+        print out
+
+def get_bugzilla_desc(bug_no):
+    # TODO: Scrape some webpages. Or find an API. Or something.
+    return 'placeholder description'
 
 if __name__ == '__main__':
     main()
