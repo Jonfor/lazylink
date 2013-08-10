fefe13d6cb54b27155382a7fa621c56e5afd1d39
Michael Comella <michael.l.comella@gmail.com>
2013-08-09 18:44:03 -0700

    Add skeleton argument parsing.

--- lazylink.py
+++ lazylink.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python2
 
+from argparse import ArgumentParser
+
 def main():
+    args = set_and_parse_args()
+    for f in args.files:
+        lazylink_file(f)
+
+def set_and_parse_args():
+    # TODO: ArgumentParser desc.
+    parser = ArgumentParser()
+    parser.add_argument('files')
+    return parser.parse_args()
+
+def lazylink_file(f):
     pass
 
 if __name__ == '__main__':
