c3894da0e5654c81331e29bf7c5602c2e848025f
Jonfor <forscherjonathan@gmail.com>
2014-08-02 13:48:37 -0400

    Added exception handling for 400 errors. Added error checking for nargs.

--- lazylink.py
+++ lazylink.py
@@ -5,7 +5,7 @@ import json
 import re
 import urllib2
 
-#BZ_URL = 'https://api-dev.bugzilla.mozilla.org/test/1.3/'
+# BZ_URL = 'https://api-dev.bugzilla.mozilla.org/test/1.3/'
 BZ_URL = 'https://api-dev.bugzilla.mozilla.org/1.3/'
 
 BUG_REG = re.compile(r'(bug\s?)([0-9]+)', re.IGNORECASE)
@@ -15,13 +15,15 @@ BUG_REG = re.compile(r'(bug\s?)([0-9]+)', re.IGNORECASE)
 # TODO: Use API to search for bugs assigned in the past week?
 def main():
     args = set_and_parse_args()
+
     for f in args.files:
         lazylink_file(f)
 
 def set_and_parse_args():
     # TODO: ArgumentParser desc.
     parser = ArgumentParser()
-    parser.add_argument('files', type=file, nargs='*')
+    parser.add_argument('files', type=file, nargs='+')
+
     return parser.parse_args()
 
 def lazylink_file(f):
@@ -34,6 +36,9 @@ def lazylink_file(f):
         bug_no = match.group(2)
         desc = get_bugzilla_desc(bug_no)
 
+        if not desc:
+            continue
+
         out = line[:match.start(1)]
         out += '{{Bug|' + bug_no + '}} - '
         out += desc
@@ -42,8 +47,12 @@ def lazylink_file(f):
 
 def get_bugzilla_desc(bug_no):
     url = BZ_URL + 'bug/' + bug_no
-    # TODO: Catch exceptions.
-    bug_info = json.load(urllib2.urlopen(url))
+    try:
+        bug_info = json.load(urllib2.urlopen(url))
+    except urllib2.HTTPError, err:
+        if err.code == 400:
+            print 'Cannot access bug #' + bug_no + '. It may be security locked.\n'
+            return ''
     return bug_info['summary']
 
 if __name__ == '__main__':
